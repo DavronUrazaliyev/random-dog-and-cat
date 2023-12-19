454e92b6ab93ff9ad857de1cf86a3f22f1919b84
Davron17 <dorazaliyev75@gmail.com>
2023-12-19 22:56:35 +0500

    dog and cat

--- handlers.py
+++ handlers.py
@@ -23,13 +23,13 @@ def echo_dog(update: Update, context: CallbackContext):
     # bot.send_photo(chat_id = user.id, photo = get_dog_image, caption = '<b><i>Tasodifiy kuchukcha :(</i></b>',parse_mode='HTML')
     update.message.reply_photo(
         photo=get_dog_image(),
-        caption= '<b><i>Tasodifiy kuchukcha :(</i></b>',
+        caption= '<b><i>Tasodifiy kuchukcha :)</i></b>',
         parse_mode='HTML'
     )
 def echo_cat(update: Update, context: CallbackContext):
     update.message.reply_photo(
         photo=get_cat_image(),
-        caption = '<b><i>Tasodifiy mushukcha :(</i></b>',
+        caption = '<b><i>Tasodifiy mushukcha :)</i></b>',
         parse_mode="HTML"
     )
 def echo_text(update: Update,context: CallbackContext):
